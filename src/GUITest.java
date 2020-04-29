921fd9d71428227b63fab0db8bf8f9f2add340dd
Clemens C <clemeli@web.de>
2020-04-29 23:29:04 +0200

    Remade GUI names with editor

--- src/GUITest.java
+++ src/GUITest.java
@@ -15,6 +15,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JSpinner;
 
 public class GUITest extends JFrame {
+	private JTextField cont;
 
 	/**
 	 * Launch the application.
@@ -37,104 +38,121 @@ public class GUITest extends JFrame {
 	 */
 	public GUITest() {
 		setTitle("Settings");
-		setBounds(100, 100, 231, 339);
+		setBounds(100, 100, 231, 372);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] {5, 0};
-		gridBagLayout.rowHeights = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30};
+		gridBagLayout.columnWidths = new int[] {5, 5};
+		gridBagLayout.rowHeights = new int[] {10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10};
 		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+		gridBagLayout.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0};
 		getContentPane().setLayout(gridBagLayout);
 		
-		JButton btnNewButton = new JButton("New button");
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnNewButton.insets = new Insets(0, 5, 0, 5);
-		gbc_btnNewButton.gridx = 0;
-		gbc_btnNewButton.gridy = 0;
-		getContentPane().add(btnNewButton, gbc_btnNewButton);
+		JButton txt = new JButton("Select Texture");
+		GridBagConstraints gbc_txt = new GridBagConstraints();
+		gbc_txt.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txt.insets = new Insets(0, 5, 5, 0);
+		gbc_txt.gridx = 0;
+		gbc_txt.gridy = 0;
+		getContentPane().add(txt, gbc_txt);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.ipady = 5;
-		gbc_lblNewLabel.insets = new Insets(-5, 5, -5, 5);
-		gbc_lblNewLabel.anchor = GridBagConstraints.NORTHWEST;
-		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 1;
-		getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+		JLabel txtSel = new JLabel("Selected: None");
+		GridBagConstraints gbc_txtSel = new GridBagConstraints();
+		gbc_txtSel.ipady = 5;
+		gbc_txtSel.insets = new Insets(-5, 5, 5, 0);
+		gbc_txtSel.anchor = GridBagConstraints.NORTHWEST;
+		gbc_txtSel.gridx = 0;
+		gbc_txtSel.gridy = 1;
+		getContentPane().add(txtSel, gbc_txtSel);
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-		gbc_btnNewButton_1.insets = new Insets(0, 5, 0, 5);
-		gbc_btnNewButton_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnNewButton_1.gridx = 0;
-		gbc_btnNewButton_1.gridy = 2;
-		getContentPane().add(btnNewButton_1, gbc_btnNewButton_1);
+		JButton img = new JButton("Select Depth Map");
+		GridBagConstraints gbc_img = new GridBagConstraints();
+		gbc_img.insets = new Insets(0, 5, 5, 0);
+		gbc_img.fill = GridBagConstraints.HORIZONTAL;
+		gbc_img.gridx = 0;
+		gbc_img.gridy = 2;
+		getContentPane().add(img, gbc_img);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.ipady = 5;
-		gbc_lblNewLabel_1.insets = new Insets(-5, 5, -5, 5);
-		gbc_lblNewLabel_1.anchor = GridBagConstraints.NORTHWEST;
-		gbc_lblNewLabel_1.gridx = 0;
-		gbc_lblNewLabel_1.gridy = 3;
-		getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
+		JLabel imgSel = new JLabel("Selected: None");
+		GridBagConstraints gbc_imgSel = new GridBagConstraints();
+		gbc_imgSel.ipady = 5;
+		gbc_imgSel.insets = new Insets(-5, 5, 5, 0);
+		gbc_imgSel.anchor = GridBagConstraints.NORTHWEST;
+		gbc_imgSel.gridx = 0;
+		gbc_imgSel.gridy = 3;
+		getContentPane().add(imgSel, gbc_imgSel);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-		gbc_lblNewLabel_2.insets = new Insets(-10, 5, -5, 5);
-		gbc_lblNewLabel_2.anchor = GridBagConstraints.SOUTHWEST;
-		gbc_lblNewLabel_2.gridx = 0;
-		gbc_lblNewLabel_2.gridy = 4;
-		getContentPane().add(lblNewLabel_2, gbc_lblNewLabel_2);
+		JLabel hntTexScale = new JLabel("Texture Scaling");
+		GridBagConstraints gbc_hntTexScale = new GridBagConstraints();
+		gbc_hntTexScale.insets = new Insets(0, 5, 0, 0);
+		gbc_hntTexScale.anchor = GridBagConstraints.SOUTHWEST;
+		gbc_hntTexScale.gridx = 0;
+		gbc_hntTexScale.gridy = 4;
+		getContentPane().add(hntTexScale, gbc_hntTexScale);
 		
-		JTextField spinner_1 = new JTextField();
-		GridBagConstraints gbc_spinner_1 = new GridBagConstraints();
-		gbc_spinner_1.insets = new Insets(0, 5, 0, 5);
-		gbc_spinner_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_spinner_1.gridx = 0;
-		gbc_spinner_1.gridy = 5;
-		getContentPane().add(spinner_1, gbc_spinner_1);
+		JTextField scale = new JTextField();
+		GridBagConstraints gbc_scale = new GridBagConstraints();
+		gbc_scale.insets = new Insets(0, 5, 5, 0);
+		gbc_scale.fill = GridBagConstraints.HORIZONTAL;
+		gbc_scale.gridx = 0;
+		gbc_scale.gridy = 5;
+		getContentPane().add(scale, gbc_scale);
 		
-		JLabel lblNewLabel_3 = new JLabel("New label");
-		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
-		gbc_lblNewLabel_3.insets = new Insets(-5, 5, -5, 5);
-		gbc_lblNewLabel_3.anchor = GridBagConstraints.SOUTHWEST;
-		gbc_lblNewLabel_3.gridx = 0;
-		gbc_lblNewLabel_3.gridy = 6;
-		getContentPane().add(lblNewLabel_3, gbc_lblNewLabel_3);
+		JLabel hntDisplacement = new JLabel("Displacement");
+		GridBagConstraints gbc_hntDisplacement = new GridBagConstraints();
+		gbc_hntDisplacement.insets = new Insets(0, 5, 0, 0);
+		gbc_hntDisplacement.anchor = GridBagConstraints.SOUTHWEST;
+		gbc_hntDisplacement.gridx = 0;
+		gbc_hntDisplacement.gridy = 6;
+		getContentPane().add(hntDisplacement, gbc_hntDisplacement);
 		
-		JTextField spinner = new JTextField();
-		GridBagConstraints gbc_spinner = new GridBagConstraints();
-		gbc_spinner.insets = new Insets(0, 5, 0, 5);
-		gbc_spinner.fill = GridBagConstraints.HORIZONTAL;
-		gbc_spinner.gridx = 0;
-		gbc_spinner.gridy = 7;
-		getContentPane().add(spinner, gbc_spinner);
+		JTextField disp = new JTextField();
+		GridBagConstraints gbc_disp = new GridBagConstraints();
+		gbc_disp.insets = new Insets(0, 5, 5, 0);
+		gbc_disp.fill = GridBagConstraints.HORIZONTAL;
+		gbc_disp.gridx = 0;
+		gbc_disp.gridy = 7;
+		getContentPane().add(disp, gbc_disp);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("New check box");
-		GridBagConstraints gbc_chckbxNewCheckBox = new GridBagConstraints();
-		gbc_chckbxNewCheckBox.insets = new Insets(0, 5, 0, 5);
-		gbc_chckbxNewCheckBox.anchor = GridBagConstraints.WEST;
-		gbc_chckbxNewCheckBox.gridx = 0;
-		gbc_chckbxNewCheckBox.gridy = 8;
-		getContentPane().add(chckbxNewCheckBox, gbc_chckbxNewCheckBox);
+		JLabel hntContrast = new JLabel("Contrast");
+		GridBagConstraints gbc_hntContrast = new GridBagConstraints();
+		gbc_hntContrast.anchor = GridBagConstraints.SOUTHWEST;
+		gbc_hntContrast.insets = new Insets(0, 5, 0, 0);
+		gbc_hntContrast.gridx = 0;
+		gbc_hntContrast.gridy = 8;
+		getContentPane().add(hntContrast, gbc_hntContrast);
 		
-		JButton btnNewButton_2 = new JButton("New button");
-		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
-		gbc_btnNewButton_2.insets = new Insets(0, 5, 0, 5);
-		gbc_btnNewButton_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnNewButton_2.gridx = 0;
-		gbc_btnNewButton_2.gridy = 9;
-		getContentPane().add(btnNewButton_2, gbc_btnNewButton_2);
+		cont = new JTextField();
+		GridBagConstraints gbc_cont = new GridBagConstraints();
+		gbc_cont.insets = new Insets(0, 5, 5, 0);
+		gbc_cont.fill = GridBagConstraints.HORIZONTAL;
+		gbc_cont.gridx = 0;
+		gbc_cont.gridy = 9;
+		getContentPane().add(cont, gbc_cont);
+		cont.setColumns(10);
 		
-		JButton btnNewButton_3 = new JButton("New button");
-		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
-		gbc_btnNewButton_3.insets = new Insets(0, 5, 0, 5);
-		gbc_btnNewButton_3.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnNewButton_3.gridx = 0;
-		gbc_btnNewButton_3.gridy = 10;
-		getContentPane().add(btnNewButton_3, gbc_btnNewButton_3);
+		JCheckBox invert = new JCheckBox("Invert Depth");
+		GridBagConstraints gbc_invert = new GridBagConstraints();
+		gbc_invert.insets = new Insets(0, 5, 5, 0);
+		gbc_invert.anchor = GridBagConstraints.WEST;
+		gbc_invert.gridx = 0;
+		gbc_invert.gridy = 10;
+		getContentPane().add(invert, gbc_invert);
+		
+		JButton go = new JButton("Render!");
+		GridBagConstraints gbc_go = new GridBagConstraints();
+		gbc_go.insets = new Insets(0, 5, 5, 0);
+		gbc_go.fill = GridBagConstraints.HORIZONTAL;
+		gbc_go.gridx = 0;
+		gbc_go.gridy = 11;
+		getContentPane().add(go, gbc_go);
+		
+		JButton save = new JButton("Save Image");
+		GridBagConstraints gbc_save = new GridBagConstraints();
+		gbc_save.insets = new Insets(0, 5, 0, 0);
+		gbc_save.fill = GridBagConstraints.HORIZONTAL;
+		gbc_save.gridx = 0;
+		gbc_save.gridy = 12;
+		getContentPane().add(save, gbc_save);
 
 	}
 }
