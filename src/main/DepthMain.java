909d675298ed4662463aca56a6fe0963809b4a12
Clemens C <clemeli@web.de>
2020-04-29 23:29:14 +0200

    Cleanup

--- src/main/DepthMain.java
+++ src/main/DepthMain.java
@@ -5,16 +5,6 @@ public class DepthMain {
     public static void main(String[] args) {
        
     	new GUI();
-    	
-//        JFrame frame = new JFrame();
-//        frame.setSize(700,700);
-//        frame.setLocationRelativeTo(null);
-//        Canvas c  = new Canvas();
-//
-//        frame.add(c);
-//        frame.setVisible(true);
-//        c.start();
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 
